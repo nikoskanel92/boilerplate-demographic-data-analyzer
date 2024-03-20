7ce1c685055874396d786942dbd44e00e92b8549
Nick Kanel <nikoskanel@hotmail.com>
2024-03-20 00:49:38 +0000

    first commit

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = None
+    df = pd.read_csv('adult.data.csv')
+    
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = None
@@ -43,27 +44,28 @@ def calculate_demographic_data(print_data=True):
     # DO NOT MODIFY BELOW THIS LINE
 
     if print_data:
-        print("Number of each race:\n", race_count) 
-        print("Average age of men:", average_age_men)
-        print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
-        print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
-        print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
-        print(f"Min work time: {min_work_hours} hours/week")
-        print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
-        print("Country with highest percentage of rich:", highest_earning_country)
-        print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
-        print("Top occupations in India:", top_IN_occupation)
+        #print("Number of each race:\n", race_count) 
+        #print("Average age of men:", average_age_men)
+        #print(f"Percentage with Bachelors degrees: {percentage_bachelors}%")
+        #print(f"Percentage with higher education that earn >50K: {higher_education_rich}%")
+        #print(f"Percentage without higher education that earn >50K: {lower_education_rich}%")
+        #print(f"Min work time: {min_work_hours} hours/week")
+        #print(f"Percentage of rich among those who work fewest hours: {rich_percentage}%")
+        #print("Country with highest percentage of rich:", highest_earning_country)
+        #print(f"Highest percentage of rich people in country: {highest_earning_country_percentage}%")
+        #print("Top occupations in India:", top_IN_occupation)
 
-    return {
-        'race_count': race_count,
-        'average_age_men': average_age_men,
-        'percentage_bachelors': percentage_bachelors,
-        'higher_education_rich': higher_education_rich,
-        'lower_education_rich': lower_education_rich,
-        'min_work_hours': min_work_hours,
-        'rich_percentage': rich_percentage,
-        'highest_earning_country': highest_earning_country,
-        'highest_earning_country_percentage':
-        highest_earning_country_percentage,
-        'top_IN_occupation': top_IN_occupation
-    }
+    return df,
+    #{
+        #'race_count': race_count,
+        #'average_age_men': average_age_men,
+        #'percentage_bachelors': percentage_bachelors,
+        #'higher_education_rich': higher_education_rich,
+        #'lower_education_rich': lower_education_rich,
+        #'min_work_hours': min_work_hours,
+        #'rich_percentage': rich_percentage,
+        #'highest_earning_country': highest_earning_country,
+        #'highest_earning_country_percentage':
+        #highest_earning_country_percentage,
+        #'top_IN_occupation': top_IN_occupation
+    #}
